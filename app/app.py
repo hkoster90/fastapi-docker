71a561276e9af684b9ec74e36dabd2056588c974
Hendrik Koster <hkoster90@sapo.pt>
2022-12-31 15:37:16 +0100

    stresstest

--- app/app.py
+++ app/app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/loaderio-683235867f40e8be022f38e69fd4ce93")
+async def loaderio_683235867f40e8be022f38e69fd4ce93():
+    return "loaderio-683235867f40e8be022f38e69fd4ce93"
