bf118d7883ba9db2b14d645daca7bdf8e97ffea9
Hendrik Koster <hkoster90@sapo.pt>
2022-12-31 15:43:12 +0100

    plaintext

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -6,6 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/loaderio-683235867f40e8be022f38e69fd4ce93")
+@app.get("/loaderio-683235867f40e8be022f38e69fd4ce93", response_class=PlainTextResponse)
 async def loaderio_683235867f40e8be022f38e69fd4ce93():
     return "loaderio-683235867f40e8be022f38e69fd4ce93"
